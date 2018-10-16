9a2903275609ba00fad28f3d9027d28a7e586e04
Moritz Umfahrer <moritz.umfahrer@hs-offenburg.de>
2018-10-16 17:28:31 +0200

    Added DynamicTextMesh string replacement to LocalizationTest

---
+++ Assets/TestScenes/UI/Localization/DynamicTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Aci.Unity.Events;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class DynamicTest : MonoBehaviour
+                         , IAciEventHandler<LocalizationChangedArgs>
+
+{
+    public int[] values;
+
+    private IAciEventManager eventMan;
+    private DynamicTextMesh dynamicText;
+
+    [Inject]
+    public IAciEventManager eventManager
+    {
+        get { return eventMan; }
+        set
+        {
+            if(eventMan != null)
+                UnregisterFromEvents();
+            eventMan = value;
+            RegisterForEvents();
+        }
+    }
+
+    /// <inheritdoc />
+    public void RegisterForEvents()
+    {
+        eventMan.AddHandler<LocalizationChangedArgs>(this);
+    }
+
+    /// <inheritdoc />
+    public void UnregisterFromEvents()
+    {
+        eventMan.RemoveHandler<LocalizationChangedArgs>(this);
+    }
+
+    /// <inheritdoc />
+    public async void OnEvent(LocalizationChangedArgs arg)
+    {
+        dynamicText = GetComponent<DynamicTextMesh>();
+        if (dynamicText == null)
+            return;
+        await Task.Delay(2000);
+        dynamicText.UpdateDynamicContent(values[0], values[1], values[2]);
+    }
+}
--- Assets/UnitTests/UI/Localization/LocalizationTest.cs
+++ Assets/UnitTests/UI/Localization/LocalizationTest.cs
@@ -5,61 +5,18 @@ using NUnit.Framework;
 using System.Collections;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Text;
 using Aci.Unity.UI.Localization;
 using Random = System.Random;
 
 public class LocalizationTest
 {
-    ILocalizationManager locMan = new LocalizationManager();
-
     [Test]
-    public void TestLocalizationLanguage([Random(10,100,3)]int totalStrings, [Values(5,10)] int numPicks)
+    public void DatabaseSubtractTest([Random(10, 30, 3)] int numSubtractDelete,
+                                     [Random(10, 30, 3)] int numSubtractRetain)
     {
+        ILocalizationManager locMan = new LocalizationManager();
         Random rand = new Random();
-        locMan.ClearData();
-        string[] keys = new string[totalStrings];
-        string[] values = new string[totalStrings];
-
-        for (int i = 0; i < totalStrings; ++i)
-        {
-            keys[i] = Guid.NewGuid().ToString();
-            values[i] = i + "_";
-        }
-
-        LocalizationData loc1 = LocalizationData.CreateInstance<LocalizationData>();
-        loc1.languageIETF = "en_US";
-        loc1.languageDescriptor = "ENG";
-        for (int i = 0; i < totalStrings; ++i)
-            loc1.stringData[keys[i]] = values[i] + "en";
-
-        LocalizationData loc2 =  LocalizationData.CreateInstance<LocalizationData>();
-        loc2.languageIETF = "de";
-        loc2.languageDescriptor = "DEU";
-        for (int i = 0; i < totalStrings; ++i)
-            loc2.stringData[keys[i]] = values[i] + "de";
-
-        locMan.AddLocalizationData(loc1);
-        locMan.AddLocalizationData(loc2);
-
-        for (int i = 0; i < numPicks; ++i)
-        {
-            int val = rand.Next(0, totalStrings);
-            string key = keys[val];
-            locMan.currentLocalization = "en_US";
-            string value1 = locMan.GetLocalized(keys[val]);
-            Assert.IsNotNull(value1);
-            locMan.currentLocalization = "de";
-            string value2 = locMan.GetLocalized(keys[val]);
-            Assert.IsNotNull(value2);
-            Assert.AreEqual(value1.Substring(0, value1.Length-3), value2.Substring(0, value2.Length - 3));
-        }
-    }
-
-    [Test]
-    public void TestLocalizationAdd([Random(10, 30, 3)] int numMatch, [Random(10, 30, 3)] int numCreate)
-    {
-        Random rand = new Random();
-        locMan.ClearData();
         int totalStrings = 60;
         string[] keys = new string[totalStrings];
         string[] values = new string[totalStrings];
@@ -71,8 +28,98 @@ public class LocalizationTest
         }
 
         LocalizationData loc1 = LocalizationData.CreateInstance<LocalizationData>();
-        loc1.languageIETF = "en_US";
-        loc1.languageDescriptor = "ENG";
+        loc1.languageIETF = "en";
+        loc1.languageDescriptor = "en";
+        for (int i = 0; i < totalStrings; ++i)
+            loc1.stringData[keys[i]] = values[i] + "en";
+
+        string[] keysToRetain = new string[numSubtractRetain];
+        int[] pickedValues = new int[numSubtractRetain];
+        for (int i = 0; i < numSubtractRetain; ++i)
+        {
+            int n = rand.Next(0, totalStrings - 1);
+            while (pickedValues.Contains(n))
+            {
+                n = rand.Next(0, totalStrings - 1);
+            }
+
+            pickedValues[i] = n;
+            keysToRetain[i] = keys[n];
+        }
+
+        LocalizationData loc2 = LocalizationData.CreateInstance<LocalizationData>();
+        loc2.languageIETF = "en";
+        loc2.languageDescriptor = "eng";
+
+        for (int i = 0; i < numSubtractRetain; ++i)
+        {
+            string key = keysToRetain[i];
+            loc2.stringData[key] = loc1.stringData[key];
+        }
+
+        locMan.AddLocalizationData(loc1);
+        locMan.AddLocalizationData(loc2);
+
+        LocalizationData loc3 = LocalizationData.CreateInstance<LocalizationData>();
+        loc3.languageIETF = "en";
+        loc3.languageDescriptor = "eng";
+
+        string[] keysToDelete = new string[numSubtractDelete];
+        int[] pickedValuesSub = new int[numSubtractDelete];
+        for (int i = 0; i < numSubtractDelete; ++i)
+        {
+            int n = rand.Next(0, totalStrings - 1);
+            while (pickedValuesSub.Contains(n) || pickedValues.Contains(n))
+            {
+                n = rand.Next(0, totalStrings - 1);
+            }
+
+            pickedValuesSub[i] = n;
+            keysToDelete[i] = keys[n];
+        }
+
+        for (int i = 0; i < numSubtractRetain; ++i)
+        {
+            string key = keysToRetain[i];
+            loc3.stringData[key] = loc1.stringData[key];
+        }
+
+        for (int i = 0; i < numSubtractDelete; ++i)
+        {
+            string key = keysToDelete[i];
+            loc3.stringData[key] = loc1.stringData[key];
+        }
+
+        locMan.RemoveLocalizationData(loc3);
+        locMan.currentLocalization = "en";
+
+        LocalizationManager testMan = (LocalizationManager) locMan;
+        //Assert.AreEqual(testMan.loadedData[0].stringData.Count, totalStrings - numSubtractDelete);
+
+        foreach (string key in keysToRetain)
+            Assert.IsNotNull(locMan.GetLocalized(key));
+        foreach (string key in keysToDelete)
+            Assert.IsNull(locMan.GetLocalized(key));
+    }
+
+    [Test]
+    public void DatabaseAddTest([Random(10, 30, 3)] int numMatch, [Random(10, 30, 3)] int numCreate)
+    {
+        ILocalizationManager locMan = new LocalizationManager();
+        Random rand = new Random();
+        int totalStrings = 60;
+        string[] keys = new string[totalStrings];
+        string[] values = new string[totalStrings];
+
+        for (int i = 0; i < totalStrings; ++i)
+        {
+            keys[i] = Guid.NewGuid().ToString();
+            values[i] = i + "_";
+        }
+
+        LocalizationData loc1 = LocalizationData.CreateInstance<LocalizationData>();
+        loc1.languageIETF = "en";
+        loc1.languageDescriptor = "eng";
         for (int i = 0; i < totalStrings; ++i)
             loc1.stringData[keys[i]] = values[i] + "en";
 
@@ -84,6 +131,7 @@ public class LocalizationTest
             {
                 n = rand.Next(0, totalStrings - 1);
             }
+
             pickedValues[i] = n;
         }
 
@@ -91,8 +139,8 @@ public class LocalizationTest
         string[] keysToCreate = new string[numCreate];
 
         LocalizationData loc2 = LocalizationData.CreateInstance<LocalizationData>();
-        loc2.languageIETF = "en_US";
-        loc2.languageDescriptor = "ENG";
+        loc2.languageIETF = "en";
+        loc2.languageDescriptor = "eng";
 
         for (int i = 0; i < numMatch; ++i)
         {
@@ -119,11 +167,62 @@ public class LocalizationTest
     }
 
     [Test]
-    public void TestLocalizationSubtract([Random(10, 30, 3)] int numSubtractDelete, [Random(10, 30, 3)] int numSubtractRetain)
+    public void LocalizedStringCombinationTest([Values("{{(.+?)}}", "<<(.+?)>>", "##(.+?)##")]string identifierRegEx
+                                             , [Values(true, false)]bool spacing)
     {
+        ILocalizationManager locMan = new LocalizationManager();
         Random rand = new Random();
-        locMan.ClearData();
-        int totalStrings = 60;
+
+        (locMan as LocalizationManager).supportedReplacementPatterns = new[] {identifierRegEx};
+        locMan.replacementPattern = identifierRegEx;
+
+        LocalizationData loc1 = LocalizationData.CreateInstance<LocalizationData>();
+        loc1.languageDescriptor = "eng";
+        loc1.languageIETF = "en";
+        loc1.stringData["TESTID_0"] = "apple";
+        loc1.stringData["TESTID_1"] = "people";
+        loc1.stringData["TESTID_2"] = "world";
+        loc1.stringData["TESTID_3"] = "car";
+
+        locMan.AddLocalizationData(loc1);
+        locMan.currentLocalization = "en";
+
+        StringBuilder bldr = new StringBuilder();
+        bldr.Append(identifierRegEx).Replace("(.+?)", "TESTID_3");
+        if (spacing)
+            bldr.Append(" ");
+        bldr.Append(identifierRegEx).Replace("(.+?)", "TESTID_1");
+        if (spacing)
+            bldr.Append(" ");
+        bldr.Append(identifierRegEx).Replace("(.+?)", "TESTID_0");
+        if (spacing)
+            bldr.Append(" ");
+        bldr.Append(identifierRegEx).Replace("(.+?)", "TESTID_2");
+        if (spacing)
+            bldr.Append(" ");
+        string locStr = bldr.ToString();
+        bldr.Clear();
+        bldr.Append(" ");
+        if (spacing)
+            bldr.Append(" ");
+        bldr.Append(" ");
+        if (spacing)
+            bldr.Append(" ");
+        bldr.Append(" ");
+        if (spacing)
+            bldr.Append(" ");
+        bldr.Append(" "); if (spacing)
+            bldr.Append(" ");
+        string expected = "carpeopleappleworld";
+        Assert.AreEqual(locMan.GetLocalized(locStr), expected);
+        bldr.Clear();
+    }
+
+    [Test]
+    public void TestLocalizationLanguage([Random(10,100,3)]int totalStrings, [Values(5,10)] int numPicks)
+    {
+        ILocalizationManager locMan = new LocalizationManager();
+        Random rand = new Random();
         string[] keys = new string[totalStrings];
         string[] values = new string[totalStrings];
 
@@ -134,74 +233,31 @@ public class LocalizationTest
         }
 
         LocalizationData loc1 = LocalizationData.CreateInstance<LocalizationData>();
-        loc1.languageIETF = "en_US";
-        loc1.languageDescriptor = "ENG";
+        loc1.languageIETF = "en";
+        loc1.languageDescriptor = "eng";
         for (int i = 0; i < totalStrings; ++i)
             loc1.stringData[keys[i]] = values[i] + "en";
 
-        string[] keysToRetain = new string[numSubtractRetain];
-        int[] pickedValues = new int[numSubtractRetain];
-        for (int i = 0; i < numSubtractRetain; ++i)
-        {
-            int n = rand.Next(0, totalStrings - 1);
-            while (pickedValues.Contains(n))
-            {
-                n = rand.Next(0, totalStrings - 1);
-            }
-            pickedValues[i] = n;
-            keysToRetain[i] = keys[n];
-        }
-
-        LocalizationData loc2 = LocalizationData.CreateInstance<LocalizationData>();
-        loc2.languageIETF = "en_US";
-        loc2.languageDescriptor = "ENG";
-
-        for (int i = 0; i < numSubtractRetain; ++i)
-        {
-            string key = keysToRetain[i];
-            loc2.stringData[key] = loc1.stringData[key];
-        }
+        LocalizationData loc2 =  LocalizationData.CreateInstance<LocalizationData>();
+        loc2.languageIETF = "de";
+        loc2.languageDescriptor = "deu";
+        for (int i = 0; i < totalStrings; ++i)
+            loc2.stringData[keys[i]] = values[i] + "de";
 
         locMan.AddLocalizationData(loc1);
         locMan.AddLocalizationData(loc2);
 
-        LocalizationData loc3 = LocalizationData.CreateInstance<LocalizationData>();
-        loc3.languageIETF = "en_US";
-        loc3.languageDescriptor = "ENG";
-
-        string[] keysToDelete = new string[numSubtractDelete];
-        int[] pickedValuesSub = new int[numSubtractDelete];
-        for (int i = 0; i < numSubtractDelete; ++i)
+        for (int i = 0; i < numPicks; ++i)
         {
-            int n = rand.Next(0, totalStrings - 1);
-            while (pickedValuesSub.Contains(n) || pickedValues.Contains(n))
-            {
-                n = rand.Next(0, totalStrings - 1);
-            }
-            pickedValuesSub[i] = n;
-            keysToDelete[i] = keys[n];
+            int val = rand.Next(0, totalStrings);
+            string key = keys[val];
+            locMan.currentLocalization = "en";
+            string value1 = locMan.GetLocalized(keys[val]);
+            Assert.IsNotNull(value1);
+            locMan.currentLocalization = "de";
+            string value2 = locMan.GetLocalized(keys[val]);
+            Assert.IsNotNull(value2);
+            Assert.AreEqual(value1.Substring(0, value1.Length-3), value2.Substring(0, value2.Length - 3));
         }
-
-        for (int i = 0; i < numSubtractRetain; ++i)
-        {
-            string key = keysToRetain[i];
-            loc3.stringData[key] = loc1.stringData[key];
-        }
-        for (int i = 0; i < numSubtractDelete; ++i)
-        {
-            string key = keysToDelete[i];
-            loc3.stringData[key] = loc1.stringData[key];
-        }
-        
-        locMan.RemoveLocalizationData(loc3);
-        locMan.currentLocalization = "en_US";
-
-        LocalizationManager testMan = (LocalizationManager)locMan;
-        //Assert.AreEqual(testMan.loadedData[0].stringData.Count, totalStrings - numSubtractDelete);
-
-        foreach (string key in keysToRetain)
-            Assert.IsNotNull(locMan.GetLocalized(key));
-        foreach (string key in keysToDelete)
-            Assert.IsNull(locMan.GetLocalized(key));
     }
 }
