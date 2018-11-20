7b84a1dc3f7682e2e9c8e92382fdc8a59ca48611
Moritz Umfahrer <moritz.umfahrer@hs-offenburg.de>
2018-11-20 14:40:57 +0100

    Added Test Scene for Camera Feed & Webcam Provider

---
+++ Assets/TestScenes/UI/ViewController/WebcamInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Aci.Unity.Events;
+using Aci.Unity.Sensor;
+using UnityEngine;
+using Zenject;
+
+public class WebcamInstaller : MonoInstaller
+{
+    public WebcamProvider webcamProvider;
+
+    public override void InstallBindings()
+    {
+        Container.Bind<IAciEventManager>().FromInstance(new AciEventManager());
+
+        Container.BindInstance<WebcamProvider>(webcamProvider);
+        Container.QueueForInject(webcamProvider);
+    }
+}
