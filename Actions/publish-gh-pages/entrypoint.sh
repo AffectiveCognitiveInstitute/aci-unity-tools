7659a7baade9a8d8ff37b2446bc15647f813d1b7
Moritz Umfahrer <moritz.umfahrer@hs-offenburg.de>
2018-12-12 16:59:21 +0100

    changed copy command in publish step

--- Actions/publish-gh-pages/entrypoint.sh
+++ Actions/publish-gh-pages/entrypoint.sh
@@ -11,7 +11,7 @@ git config --global user.name "$GH_USER"
 git clone --depth 1 --branch ${GH_PAGES_BRANCH} --single-branch https://x-access-token:${GITHUB_TOKEN}@${GH_REPO} ../docs-repo
 
 # Publish docs
-cp -a -r ${CONTENT} ../docs-repo/
+for file in ${CONTENT}; do cp -a -r "$file" ../docs-repo/; done
 
 cd ../docs-repo/
 
