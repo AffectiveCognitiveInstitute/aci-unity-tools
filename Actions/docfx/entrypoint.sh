696452f7870adbe83a58fde6819218e3961b00b3
Moritz Umfahrer <moritz.umfahrer@hs-offenburg.de>
2018-12-12 15:35:47 +0100

    fixed chmod

--- Actions/docfx/entrypoint.sh
+++ Actions/docfx/entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/sh -l
 
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+# Install dependencies
+apt-get update \
+	&& apt-get install -y gnupg gnupg2 gnupg1
+
+# Install MONO
+apt-get install -y apt-transport-https dirmngr
+apt-key adv --no-tty --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/debian stable-stretch main" | tee /etc/apt/sources.list.d/mono-official-stable.list \
+  && apt-get update \
+  && apt-get install -y mono-devel \
+&& rm -rf /var/lib/apt/lists/* /tmp/*
+
 # Install DocFX
 apt-get update
 apt-get install -y unzip wget
--- Actions/publish-gh-pages/entrypoint.sh
+++ Actions/publish-gh-pages/entrypoint.sh
@@ -8,7 +8,7 @@ apt-get install -y git
 git config --global user.email "$GH_EMAIL"
 git config --global user.name "$GH_USER"
 
-git clone ${GH_REPO} --depth 1 --branch gh-pages --single-branch ../docs-repo
+git clone ${GH_REPO} --depth 1 --branch ${GH_PAGES_BRANCH} --single-branch ../docs-repo
 
 # Publish docs
 cp -a -r ${CONTENT} ../docs-repo/
