225a685bc6a2a2a88eb8f4b0176975eb3bdbe8bd
Moritz Umfahrer <moritz.umfahrer@hs-offenburg.de>
2018-12-12 17:11:22 +0100

    added debug output

--- Actions/publish-gh-pages/entrypoint.sh
+++ Actions/publish-gh-pages/entrypoint.sh
@@ -11,7 +11,10 @@ git config --global user.name "$GH_USER"
 git clone --depth 1 --branch ${GH_PAGES_BRANCH} --single-branch https://x-access-token:${GITHUB_TOKEN}@${GH_REPO} ../docs-repo
 
 # Publish docs
-for file in ${CONTENT}; do cp -a -r "$file" ../docs-repo/; done
+for file in ${CONTENT}; do 
+	echo "$file"
+	cp -a -r "$file" ../docs-repo/
+done
 
 cd ../docs-repo/
 
