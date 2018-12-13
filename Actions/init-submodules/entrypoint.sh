9abd43a6fb349582062b65b2cb12dd07ccb726a6
Moritz Umfahrer <moritz.umfahrer@hs-offenburg.de>
2018-12-13 18:57:18 +0100

    checking with location at /root/

--- Actions/init-submodules/entrypoint.sh
+++ Actions/init-submodules/entrypoint.sh
@@ -5,14 +5,13 @@ apt-get update
 apt-get install -y git
 
 # update rsa
-mkdir ~/.ssh
-chmod 700 ~/.ssh
+mkdir /root/.ssh
+chmod 700 /root/.ssh
 eval $(ssh-agent -s)
-echo "${ACITOOLS_SSH}" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-ssh-add ~/.ssh/id_rsa
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+echo "${ACITOOLS_SSH}" > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+ssh-add /root/.ssh/id_rsa
+ssh-keyscan github.com >> /root/.ssh/known_hosts
 ssh -Tv git@github.com
 
 # try submodules
