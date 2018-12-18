11cfd5c3a32196df7ded1ce61feb68c30353b78e
Moritz Umfahrer <moritz.umfahrer@hs-offenburg.de>
2018-12-18 11:05:07 +0100

    Update to Unity 2018.3 + updated submodule and network test

--- Assets/TestScenes/Network/MessageHandler.cs
+++ Assets/TestScenes/Network/MessageHandler.cs
@@ -5,9 +5,13 @@ using Aci.Unity.Logging;
 using Aci.Unity.Network;
 using UnityEngine;
 
-public class MessageHandler : NetworkPackageHandlerBase<EmotionPackage> {
+public class MessageHandler : NetworkPackageHandlerBase<EmotionPackage, LoggingPackage> {
     
     public override void Handle(EmotionPackage package)
+    {
+        AciLog.Log("MessageHandler", "I handled a message!");
+    }
+    public override void Handle(LoggingPackage package)
     {
         AciLog.Log("MessageHandler", "I handled a message!");
     }
