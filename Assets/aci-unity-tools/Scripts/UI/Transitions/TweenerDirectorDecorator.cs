657f7810833ee9b3742d5a38f1667c60a2eb0f92
James Gay <g.james@gmx.net>
2019-09-04 14:51:33 +0200

    added tweener transition

---
+++ Assets/aci-unity-tools/Scripts/UI/Transitions/TweenerDirectorDecorator.cs
@@ -0,0 +1,13 @@
+﻿using Aci.Unity.UI.Tweening;
+namespace Aci.Unity.UI
+{
+    [System.Serializable]
+    public class TweenerDirectorDecorator
+    {
+        public bool playReverse;
+        public TweenerDirector director;
+    }
+}
+
+
+
---
+++ Assets/aci-unity-tools/Scripts/UI/Transitions/TweenerTransition.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Aci.Unity.UI
+{
+    public class TweenerTransition : MonoBehaviour, ITransition
+    {
+        [SerializeField]
+        private TweenerDirectorDecorator m_EnterGroup;
+
+        [SerializeField]
+        private TweenerDirectorDecorator m_ExitGroup;
+
+        public Task EnterAsync()
+        {
+            if (m_EnterGroup != null)
+                return ExecuteTween(m_EnterGroup);
+
+            return Task.CompletedTask;
+        }
+
+        public Task ExitAsync()
+        {
+            if (m_ExitGroup != null)
+                return ExecuteTween(m_ExitGroup);
+
+            return Task.CompletedTask;
+        }        
+
+        private Task ExecuteTween(TweenerDirectorDecorator tweenerDirector)
+        {
+            return tweenerDirector.playReverse ? tweenerDirector.director.PlayReverseAsync() : tweenerDirector.director.PlayForwardsAsync();
+        }
+    }
+}
