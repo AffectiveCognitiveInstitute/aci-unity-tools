7a8b41a125432aa0fd2a103eb1201d60b20d1749
James Gay <g.james@gmx.net>
2019-09-04 14:27:17 +0200

    added TweenerScreenTransition

---
+++ Assets/aci-unity-tools/Scripts/Editor/UI/Navigation/TweenerScreenTransitionEditor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Aci.Unity.UI.Navigation
+{
+    [CustomEditor(typeof(TweenerScreenTransition))]
+    public class TweenerScreenTransitionEditor : Editor
+    {
+        private SerializedProperty m_UseSameTweenForReturnAndDestroy;
+        private SerializedProperty m_EnterTween;
+        private SerializedProperty m_ReturnTween;
+        private SerializedProperty m_LeavingTween;
+        private SerializedProperty m_DestroyTween;
+
+        private void OnEnable()
+        {
+            m_UseSameTweenForReturnAndDestroy = serializedObject.FindProperty("m_UseSameTweenForReturnAndDestroy");
+            m_EnterTween = serializedObject.FindProperty("m_EnterTween");
+            m_ReturnTween = serializedObject.FindProperty("m_ReturnTween");
+            m_LeavingTween = serializedObject.FindProperty("m_LeavingTween");
+            m_DestroyTween = serializedObject.FindProperty("m_DestroyedTween");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            EditorGUILayout.HelpBox("Check this box if you want to use the same animation when entering and returning, and also for leaving and destroying", MessageType.Info);
+
+            EditorGUILayout.PropertyField(m_UseSameTweenForReturnAndDestroy, new GUIContent("Use same Tweens"));
+
+            if (m_UseSameTweenForReturnAndDestroy.boolValue)
+            {
+                EditorGUILayout.PropertyField(m_EnterTween, true);
+                EditorGUILayout.PropertyField(m_LeavingTween, true);
+            }
+            else
+            {
+                EditorGUILayout.PropertyField(m_EnterTween, true);
+                EditorGUILayout.PropertyField(m_LeavingTween, true);
+                EditorGUILayout.PropertyField(m_ReturnTween, true);
+                EditorGUILayout.PropertyField(m_DestroyTween, true);
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
+
---
+++ Assets/aci-unity-tools/Scripts/UI/Navigation/Transitions/ScreenTransition.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Aci.Unity.UI.Navigation
+{
+    public abstract class ScreenTransition : MonoBehaviour, IScreenTransition
+    {
+        public virtual Task MakeTransitionAsync(NavigationMode navigationMode)
+        {
+            switch (navigationMode)
+            {
+                case NavigationMode.Entering:
+                    return OnScreenEntering();
+                case NavigationMode.Leaving:
+                    return OnScreenLeaving();
+                case NavigationMode.Returning:
+                    return OnScreenReturning();
+                case NavigationMode.Removed:
+                    return OnScreenBeingRemoved();
+                default:
+                    throw new System.ArgumentOutOfRangeException(nameof(navigationMode));
+            }
+        }
+
+        public abstract Task OnScreenEntering();
+        public abstract Task OnScreenLeaving();
+        public abstract Task OnScreenReturning();
+        public abstract Task OnScreenBeingRemoved();
+        public abstract void DisplayImmediately();
+    }
+}
