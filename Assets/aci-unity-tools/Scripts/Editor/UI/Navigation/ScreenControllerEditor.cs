42a3aecf25ad689e26defb04a4d02dc98e7dd229
James Gay <g.james@gmx.net>
2019-09-04 13:20:07 +0200

    added ScreenController implementation

---
+++ Assets/aci-unity-tools/Scripts/Editor/UI/Navigation/ScreenControllerEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEditor;
+
+namespace Aci.Unity.UI.Navigation
+{
+    [CustomEditor(typeof(ScreenController))]
+    public class ScreenControllerEditor : Editor
+    {
+        private SerializedProperty m_PrefabLoadingStrategy;
+        private SerializedProperty m_Prefab;
+        private SerializedProperty m_PrefabPath;
+
+        private void OnEnable()
+        {
+            m_PrefabLoadingStrategy = serializedObject.FindProperty("m_PrefabLoadingStrategy");
+            m_Prefab = serializedObject.FindProperty("m_Prefab");
+            m_PrefabPath = serializedObject.FindProperty("m_PrefabPath");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            DrawPropertiesExcluding(serializedObject, "m_Script", "m_Prefab", "m_PrefabPath");
+
+            EditorGUILayout.PropertyField(m_PrefabLoadingStrategy.enumValueIndex == (int)ScreenController.PrefabLoadingStrategy.FromReference ?
+                                            m_Prefab : m_PrefabPath);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
+
---
+++ Assets/aci-unity-tools/Scripts/UI/Navigation/ScreenController.cs
@@ -0,0 +1,151 @@
+﻿using Aci.Unity.Events;
+using System.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+
+namespace Aci.Unity.UI.Navigation
+{
+    public class ScreenController : MonoBehaviour, IScreenController
+    {
+        public enum PrefabLoadingStrategy
+        {
+            FromReference,
+            FromResource
+        }
+
+        [SerializeField]
+        private string m_Id;
+
+        [SerializeField]
+        private PrefabLoadingStrategy m_PrefabLoadingStrategy;
+
+        [SerializeField]
+        private GameObject m_Prefab;
+
+        [SerializeField]
+        private string m_PrefabPath;
+
+        private GameObject m_Instance;
+        private IScreenRegistry m_ScreenRegistry;
+        private IAciEventManager m_EventManager;
+        private IInstantiator m_Instantiator;
+        private IScreenTransition m_ScreenTransition;
+        public string id => m_Id;
+
+        [Inject]
+        public void Construct(IScreenRegistry screenRegistry,
+                              IInstantiator instantiator,
+                              IAciEventManager eventManager)
+        {
+            m_ScreenRegistry = screenRegistry;
+            m_EventManager = eventManager;
+            m_Instantiator = instantiator;
+        }
+
+        private void OnEnable()
+        {
+            m_ScreenRegistry.Register(this);
+        }
+
+        private void OnDisable()
+        {
+            m_ScreenRegistry.Unregister(this);
+        }
+
+        public void Prepare()
+        {
+            if(m_Instance == null)
+            {
+                m_Instance = InstantiateScreen();
+                m_ScreenTransition = m_Instance.GetComponent<IScreenTransition>();
+                m_Instance.transform.SetParent(transform, false);
+                m_Instance.name = m_Id;
+                m_Instance.SetActive(false);
+            }
+        }
+
+        private GameObject InstantiateScreen()
+        {
+            if (m_PrefabLoadingStrategy == PrefabLoadingStrategy.FromResource)
+                m_Prefab = Resources.Load<GameObject>(m_PrefabPath);
+
+            return m_Instantiator.InstantiatePrefab(m_Prefab);
+        }
+
+        public bool Equals(IScreenController other)
+        {
+            return other.id == id;
+        }
+
+        public void OnNavigatedTo(INavigationParameters navigationParameters)
+        {
+            ExecuteNavigationEvent.Execute(m_Instance, ExecuteNavigationEvent.navigatedTo, navigationParameters);
+        }
+
+        public void OnNavigatingAway(INavigationParameters navigationParameters)
+        {
+            ExecuteNavigationEvent.Execute(m_Instance, ExecuteNavigationEvent.navigatingAway, navigationParameters);
+        }
+
+        public void OnNavigatingBack(INavigationParameters navigationParameters)
+        {
+            ExecuteNavigationEvent.Execute(m_Instance, ExecuteNavigationEvent.navigatingBack, navigationParameters);
+        }
+
+        public void OnNavigatedBack(INavigationParameters navigationParameters)
+        {
+            ExecuteNavigationEvent.Execute(m_Instance, ExecuteNavigationEvent.navigatedBack, navigationParameters);
+        }
+
+        public void OnNavigatingTo(INavigationParameters navigationParameters)
+        {
+            ExecuteNavigationEvent.Execute(m_Instance, ExecuteNavigationEvent.navigatingTo, navigationParameters);
+        }
+
+        public void OnScreenDestroyed(INavigationParameters navigationParameters)
+        {
+            ExecuteNavigationEvent.Execute(m_Instance, ExecuteNavigationEvent.destroyed, navigationParameters);
+        }
+
+        public async Task UpdateDisplayAsync(NavigationMode navigationMode, bool animated = true)
+        {
+            Prepare();
+            bool willAnimate = animated && m_ScreenTransition != null;
+
+            m_EventManager.Invoke(new NavigationModeChangedEvent(this.m_Id, navigationMode));
+
+            if(!willAnimate)
+            {
+                switch (navigationMode)
+                {
+                    case NavigationMode.Entering:
+                    case NavigationMode.Returning:
+                        m_Instance.SetActive(true);
+                        if (m_ScreenTransition != null)
+                            m_ScreenTransition.DisplayImmediately();
+                        break;
+                    case NavigationMode.Leaving:
+                        m_Instance.SetActive(false);
+                        break;
+                    case NavigationMode.Removed:
+                        Destroy(m_Instance);
+                        m_Instance = null;
+                        break;
+                }
+            }
+            else
+            {
+                if (navigationMode == NavigationMode.Entering || navigationMode == NavigationMode.Returning)
+                    m_Instance.SetActive(true);
+
+                await m_ScreenTransition.MakeTransitionAsync(navigationMode);
+
+                if (navigationMode == NavigationMode.Removed)
+                    Destroy(m_Instance);
+                else if (navigationMode == NavigationMode.Leaving)
+                    m_Instance.SetActive(false);
+            }
+        }
+
+    }
+}
