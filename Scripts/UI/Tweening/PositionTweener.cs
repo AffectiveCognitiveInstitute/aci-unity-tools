a3d5a34bf2b8b232c358c23a58c22b96e482c5cd
Moritz Umfahrer <moritz.umfahrer@hs-offenburg.de>
2020-02-13 08:21:55 +0100

    Automated release for UnityPackageManager with version 1.1.17

---
+++ Scripts/UI/Tweening/PositionTweener.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Aci.Unity.UI.Tweening
+{
+    public class PositionTweener : Tweener<Transform, Vector3>
+    {
+        protected override void ExecuteFrame(float percentage)
+        {
+            float t = m_Transition.Evaluate(percentage);
+            m_Target.position = Vector3.LerpUnclamped(fromValue, toValue, t);
+        }
+    }
+}
